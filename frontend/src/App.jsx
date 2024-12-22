6895e14433e7f14430b9307303a949b1964c7b9a
ramani hasti <ramanihasti053@gmail.com>
2024-12-22 13:51:42 +0530

    started with frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "flowbite-react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="font-serif text-xl">Hello World</h2>
+      <Button>Click me</Button>
+      {/* //react-route-dom //tailwind css //flowbite-react //vite */}
+    </>
+  );
+}
+
+export default App;
