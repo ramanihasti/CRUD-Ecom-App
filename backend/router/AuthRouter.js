170a4b9fdad8d269ed28eb6ecf4fbf170c7823ba
ramani hasti <ramanihasti053@gmail.com>
2025-04-01 18:30:49 +0530

    Auth router

--- backend/router/AuthRouter.js
+++ backend/router/AuthRouter.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const { register, login, logout } = require("../controller/AuthController");
-const { authMiddlewares } = require("../middlewares/authMiddlewares");
+const {
+  register,
+  login,
+  logout,
+  getUser,
+} = require("../controller/AuthController");
+const {
+  authMiddlewares,
+  adminAuthMiddlewares,
+} = require("../middlewares/authMiddlewares");
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.get("/logout", authMiddlewares, logout);
+authRouter.get("/getUser", authMiddlewares, getUser);
+authRouter.get("/getAdmin", adminAuthMiddlewares, getUser);
 
 module.exports = authRouter;
