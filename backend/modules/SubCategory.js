f937af41d03ce937cecc8adef42e6c31374bf252
ramani hasti <ramanihasti053@gmail.com>
2025-04-01 18:23:33 +0530

    sub category model

--- backend/modules/SubCategory.js
+++ backend/modules/SubCategory.js
@@ -3,11 +3,17 @@ const mongoose = require("mongoose");
 const subCategorySchema = new mongoose.Schema({
   // _id: { type: mongoose.Types.ObjectId("6773e5bc8c8edae37094d07e") },
   // id: { type: mongoose.Types.ObjectId },
-  name: { type: String, minLength: 2, required: true },
-  slug: { type: String, minLength: 2, required: true, unique: true },
+  name: { type: String, minLength: 2, maxLength: 100, required: true },
+  slug: {
+    type: String,
+    minLength: 2,
+    maxLength: 100,
+    required: true,
+    unique: true,
+  },
   image: { type: String, required: true },
   category: {
-    type: [mongoose.Types.ObjectId],
+    type: mongoose.Types.ObjectId,
     ref: "Category",
     required: true,
   },
