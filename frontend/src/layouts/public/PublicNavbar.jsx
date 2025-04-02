9f5d03fadaca42b4cc8bb39a08e195cfdb3e9994
ramani hasti <ramanihasti053@gmail.com>
2025-04-02 19:49:13 +0530

    public layout set the page

--- frontend/src/layouts/public/PublicNavbar.jsx
+++ frontend/src/layouts/public/PublicNavbar.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import { COMPANY_NAME } from "../../consts";
-import { getAllPages } from "../../services/apiServices";
+import { getAllPages, logout } from "../../services/apiServices";
 import { toast } from "react-toastify";
 import { HiShoppingCart } from "react-icons/hi2";
+import CartDrawer from "../../components/public/cart/CartDrawer";
 
 function PublicNavbar() {
   const [pages, setPages] = useState([]);
@@ -18,94 +19,128 @@ function PublicNavbar() {
     setIsOpen(true);
   }
 
-  function handlecClose() {
+  function handleClose() {
     setIsOpen(false);
   }
 
-  async function fetchAllPages() {
+  async function fetchPages() {
     try {
       const result = await getAllPages();
 
       if (!result.success) {
         toast("Failed to fetch pages.", { type: "error" });
+        console.log(result.msg);
+        return;
       }
-      setPages(result.data);
+
+      const temp = result.data.map((page) => {
+        return {
+          id: page._id,
+          name: page.name,
+          slug: page.slug,
+        };
+      });
+
+      setPages(temp);
     } catch (error) {
       toast("Failed to fetch pages.", { type: "error" });
+      console.log(error.message);
     }
   }
   useEffect(() => {
-    fetchAllPages();
+    fetchPages();
   }, []);
 
+  async function handleLogout() {
+    try {
+      const result = await logout();
+
+      if (!result.success) {
+        alert(result.msg);
+        return;
+      }
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      alert("Logged out successfully.");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Navbar fluid border>
-      <Navbar.Brand as={"div"}>
-        <img
-          src="/logo.png"
-          className="mr-3 h-6 sm:h-9"
-          alt={`${COMPANY_NAME} Logo`}
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          {COMPANY_NAME}
-        </span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button size="sm" pill className="mr-2" onClick={handleOpen}>
-          <span className="flex items-center justify-center">
-            <HiShoppingCart className="h-5 w-5" />
+    <>
+      <Navbar fluid border>
+        <Navbar.Brand href="/">
+          <img
+            src="/logo.png"
+            className="mr-3 h-6 sm:h-9"
+            alt={`${COMPANY_NAME} Logo`}
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            {COMPANY_NAME}
           </span>
-        </Button>
-        {isLoggedIn ? (
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar
-                alt="User settings"
-                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                rounded
-              />
-            }
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">{user.fname}</span>
-              <span className="block truncate text-sm font-medium">
-                {user.email}
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Item as={Link} to="/admin">
-              Dashboard
-            </Dropdown.Item>
-            <Dropdown.Item as={Link} to="/user">
-              Account
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item>Log Out</Dropdown.Item>
-          </Dropdown>
-        ) : (
-          <Button size="sm" psill as={Link} to="/login" className="mr-2">
-            Login/Register
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Button size="sm" pill className="mr-2" onClick={handleOpen}>
+            <span className="flex items-center justify-center">
+              <HiShoppingCart className="h-5 w-5" />
+            </span>
           </Button>
-        )}
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        {pages.map((page) => {
-          return (
-            <Navbar.Link key={page._id} href={`/${page.slug}`}>
+          {isLoggedIn ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar
+                  alt="User settings"
+                  img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                  rounded
+                />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">{user.fullname}</span>
+                <span className="block truncate text-sm font-medium">
+                  {user.email}
+                </span>
+              </Dropdown.Header>
+              {user.role === "admin" && (
+                <Dropdown.Item as={Link} to="/admin">
+                  Dashboard
+                </Dropdown.Item>
+              )}
+              <Dropdown.Item as={Link} to="/user">
+                Account
+              </Dropdown.Item>
+              <Dropdown.Item onClick={handleLogout}>Log Out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Button size="sm" pill as={Link} to="/login" className="mr-2">
+              Login/Register
+            </Button>
+          )}
+          <Navbar.Toggle />
+        </div>
+
+        <Navbar.Collapse>
+          <Navbar.Link as={Link} to="/">
+            Home
+          </Navbar.Link>
+          {pages.map((page) => (
+            <Navbar.Link key={page.id} as={Link} to={`/${page.slug}`}>
               {page.name}
             </Navbar.Link>
-          );
-        })}
-        <Navbar.Link as={Link} to="/contact">
-          Contact
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+          ))}
+          <Navbar.Link as={Link} to="/contact">
+            Contact
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <CartDrawer isOpen={isOpen} handleClose={handleClose} />
+    </>
   );
 }
 
